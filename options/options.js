f170f33dac3a42cd6e2afbf734cdda467f61ca22
Crowdbotics <team@crowdbotics.com>
2021-12-08 10:48:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "timnee_mobile_32312",
+  url: "https://timnee_mobile_32312.botics.co",
+  api: "https://timnee_mobile_32312.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
